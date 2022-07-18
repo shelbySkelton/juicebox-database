478c7f97bee454996e98dd6560e8f56786a1ddab
Shelby Skelton <shelbyskelton09@gmail.com>
2022-07-18 16:13:57 -0400

    part 1 of part 1 commit

--- db/seed.js
+++ db/seed.js
@@ -11,8 +11,9 @@ async function createInitialUsers() {
     console.log("Starting to create users...");
 
     const albert = await createUser({ username: 'albert', password: 'bertie99' });
+    const sandra = await createUser({ username: 'sandra', password: '2sandy4me' });
+    const glamgal = await createUser({ username: 'glamgal', password: 'soglam' });
 
-    console.log(albert);
 
     console.log("Finished creating users!");
   } catch(error) {
